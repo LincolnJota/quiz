3a8922804492dac6749a0a6ce08c76ce4fccf8f6
LincolnJota <contato.joydamasceno@gmail.com>
2022-07-31 02:29:04 -0300

    🎨 Removal of useless codes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,45 +6,13 @@ import Quiz from '../components/Quiz';
 import AnswerModel from '../models/answer'
 import QuestionModel from '../models/question'
 
-const questionMock = new QuestionModel(1, "Melhor Cor?", [
-  AnswerModel.incorrect("Vermelho"),
-  AnswerModel.incorrect("Azul"),
-  AnswerModel.correct("Verde"),
-  AnswerModel.incorrect("Amarelo")
-]);
-
 const BASE_URL = 'http://localhost:3000/api';
-
-const text = `
-{
-	"id": 204,
-	"statement": "Qual é o triângulo que tem todos os lados diferentes?",
-	"answered": false,
-	"correctAnswer": false,
-	"answers": [{
-		"value": "Equilátero",
-		"correct": false,
-		"revealed": false
-	}, {
-		"value": "Escaleno",
-		"correct": true,
-		"revealed": false
-	}, {
-		"value": "Trapézio",
-		"correct": false,
-		"revealed": false
-	}, {
-		"value": "Isóceles",
-		"correct": false,
-		"revealed": false
-	}]
-}`;
 
 export default function Home() {
   const router = useRouter();
 
   const [questionIds, setQuestionIds] = useState<number[]>([]);
-  const [question, setQuestion] = useState<QuestionModel>(questionMock);
+  const [question, setQuestion] = useState<QuestionModel>();
   const [correctAnswers, setCorrectAnswers] = useState<number>(0);
 
 
